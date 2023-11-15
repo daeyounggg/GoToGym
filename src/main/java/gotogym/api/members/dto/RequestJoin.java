e666de7b834456200e8c45730edd95f01fa70c97
yonggyo1125 <yonggyo00@kakao.com>
2023-11-15 23:05:27 +0900

    조별 포트폴리오 작업을 위핸 백앤드 서버 기초 구현

---
+++ src/main/java/gotogym/api/members/dto/RequestJoin.java
@@ -0,0 +1,25 @@
+package gotogym.api.members.dto;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RequestJoin(
+        @NotBlank @Email
+        String email,
+
+        @NotBlank
+        String password,
+
+        @NotBlank
+        String confirmPassword,
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        String mobile,
+
+        @AssertTrue
+        boolean agree
+) {}
