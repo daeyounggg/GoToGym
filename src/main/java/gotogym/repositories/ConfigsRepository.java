7d897fe83046e19dd75861276c54d89783876cad
yonggyo1125 <yonggyo00@kakao.com>
2023-11-15 22:44:54 +0900

    re setting

--- src/main/java/gotogym/repositories/ConfigsRepository.java
+++
@@ -1,7 +0,0 @@
-package gotogym.repositories;
-
-import gotogym.entities.Configs;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfigsRepository extends JpaRepository<Configs, String> {
-}
