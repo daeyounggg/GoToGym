5c1356bb6c2eab32bfec0b6238dc37376c1acd2c
yonggyo1125 <yonggyo00@kakao.com>
2023-11-15 22:44:54 +0900

    re setting

--- src/main/java/gotogym/commons/exceptions/AlertException.java
+++
@@ -1,9 +0,0 @@
-package gotogym.commons.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class AlertException extends CommonException {
-    public AlertException(String message) {
-        super(message, HttpStatus.BAD_REQUEST);
-    }
-}
