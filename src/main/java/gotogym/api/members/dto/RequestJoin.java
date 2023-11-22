7123476890d8cc515eed388c1e61fcd44e75d546
yonggyo1125 <yonggyo00@kakao.com>
2023-11-22 17:58:03 +0900

    관리자 페이지 기본 구성 완료

--- src/main/java/gotogym/api/members/dto/RequestJoin.java
+++ src/main/java/gotogym/api/members/dto/RequestJoin.java
@@ -3,20 +3,15 @@ package gotogym.api.members.dto;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-<<<<<<< HEAD
 import jakarta.validation.constraints.Size;
-=======
->>>>>>> support
+
 
 public record RequestJoin(
         @NotBlank @Email
         String email,
 
         @NotBlank
-<<<<<<< HEAD
         @Size(min=8)
-=======
->>>>>>> support
         String password,
 
         @NotBlank
