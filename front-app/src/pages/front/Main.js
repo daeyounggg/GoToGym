43d9384bc82f593c5a422d5f127f10847bdf22eb
진혁 <kitty0888@naver.com>
2023-11-25 23:45:42 +0900

    관리자 및 회원가입 기능 구현 진행중

--- front-app/src/pages/front/Main.js
+++ front-app/src/pages/front/Main.js
@@ -1,5 +1,12 @@
+import FileUploader from '../../components/commons/FileUploader';
+
 const Main = () => {
-  return <h1>메인페이지</h1>;
+  return (
+    <>
+      <FileUploader>파일업로드</FileUploader>
+      <h1>메인페이지</h1>
+    </>
+  );
 };
 
 export default Main;
