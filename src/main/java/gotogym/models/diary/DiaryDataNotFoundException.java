36f8debe13337ae97864379cdfd65f4d6c9b9e80
daeyounggg <137499608+daeyounggg@users.noreply.github.com>
2023-11-27 22:11:17 +0900

    Update DiaryDataNotFoundException.java

--- src/main/java/gotogym/models/diary/DiaryDataNotFoundException.java
+++ src/main/java/gotogym/models/diary/DiaryDataNotFoundException.java
@@ -3,6 +3,7 @@ package gotogym.models.diary;
 import gotogym.commons.exceptions.AlertBackException;
 import org.springframework.http.HttpStatus;
 
+@Setter
 public class DiaryDataNotFoundException extends AlertBackException {
     public DiaryDataNotFoundException() {
         super("등록되지 않은 게시글 입니다.");
